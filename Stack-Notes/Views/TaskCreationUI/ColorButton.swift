0171a92e11732eff21a36ea90c40717597d17389
Jackson Ho <mintri1199@gmail.com>
2019-06-18 12:33:59 -0700

    [Modified] Share schemes for bitrise

--- Stack-Notes/Views/TaskCreationUI/ColorButton.swift
+++ Stack-Notes/Views/TaskCreationUI/ColorButton.swift
@@ -19,7 +19,6 @@ class ColorButton: UIButton {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
     override func didMoveToWindow() {
         layer.addSublayer(circleLayer)
         layer.addSublayer(borderLayer)
